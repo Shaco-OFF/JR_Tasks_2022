e3ea0f09e1db3af9264e093039fdf390d77bb3db
Shaco-OFF <shacojava@gmail.com>
2022-08-10 15:52:53 +0300

    1819

---
+++ task1819/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task18.task1819;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Объединение файлов
+C:/Users/Shaco/Desktop/first.txt
+C:/Users/Shaco/Desktop/second.txt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFileName = reader.readLine();
+        String secondFileName = reader.readLine();
+
+        List<Integer> listBytes = new ArrayList<>();
+        int data;
+
+        try(BufferedReader readerFirst = new BufferedReader(new FileReader(firstFileName));
+            BufferedReader readerSecond = new BufferedReader(new FileReader(secondFileName))) {
+
+            while (readerSecond.ready()) {
+                data = readerSecond.read();
+                listBytes.add(data);
+            }
+            while (readerFirst.ready()) {
+                data = readerFirst.read();
+                listBytes.add(data);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(firstFileName))) {
+            for (Integer b : listBytes) {
+                writer.write(b);
+            }
+        }
+    }
+}
