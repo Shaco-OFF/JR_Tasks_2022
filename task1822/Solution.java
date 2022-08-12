13643b49d480ebe2e1ce9936f7a88b972a0bf54f
Shaco-OFF <shacojava@gmail.com>
2022-08-12 13:41:14 +0300

    1822

--- task1822/Solution.java
+++ task1822/Solution.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 /* 
 Поиск данных внутри файла
