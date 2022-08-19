85b99dbdae39a3aaf7204f79666c5b8bf7cc7a67
Shaco-OFF <shacojava@gmail.com>
2022-08-19 10:21:39 +0300

    Удаление всех знаков препинания в файле

--- task1910/Solution.java
+++ task1910/Solution.java
@@ -2,6 +2,7 @@ package task1910;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /* 
 Пунктуация
@@ -15,12 +16,32 @@ public class Solution {
             BufferedReader reader = new BufferedReader(new FileReader(readerFileName.readLine()));
             BufferedWriter writer = new BufferedWriter(new FileWriter(readerFileName.readLine()))) {
 
+            long time = System.currentTimeMillis();
+
+            /*
+            //regex решение
             String line;
             while ((line = reader.readLine()) != null) {
                 writer.write(line.replaceAll("\\p{Punct}", ""));
             }
+             */
+            //функциональное решение
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(delNoDigOrLet(line));
+            }
+
+            System.out.println(System.currentTimeMillis() - time);
+
         }catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    //код без использования regex почти в 2 раза быстрее
+    private static String delNoDigOrLet(String str) {
+        return str.chars()
+                .filter(Character::isLetterOrDigit)
+                .mapToObj(i -> String.valueOf((char) i))
+                .collect(Collectors.joining());
     }
 }
